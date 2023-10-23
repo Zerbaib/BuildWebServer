a7eec4ae8fedbaaa40449d3988344d3e0d2c66d4
Zerbaib <hugo.hennetin@code.catdev.fr>
2023-10-23 14:46:07 +0000

    patch

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@ sudo apt update && sudo apt full-upgrade -y
 sudo apt install git -y
 
 sudo git clone https://github.com/Zerbaib/BuildWebServer.git
-sudo cd BuildWebServer
-sudo chmod 777 ./installer.sh
-sudo ./installer.sh
+sudo chmod 777 ./BuildWebServer/installer.sh
+sudo ./BuildWebServer/installer.sh
